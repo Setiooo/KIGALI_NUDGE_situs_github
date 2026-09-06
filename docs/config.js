94d9533fdd1d5c41d561e13fafdc5fbfdc185048
Setiooo <budibohcil@gmail.com>
2026-09-06 08:14:26 +0700

    Update KIGALI NUDGE realtime

--- docs/config.js
+++ docs/config.js
@@ -2,9 +2,7 @@
    KONFIGURASI SITUS KIGALI NUDGE
    ============================================================ */
 window.KIGALI_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbz18Z0z_6d7vgyuvPRJhFasLR_b5cWBcDjIQpQVTNB4_9ruO9KALZ5r826V1HIyWf9s/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyd04k9EJgT0ib8NTeM0zI-fn0pnk5FRh-8A56UTfN1azf1P3nwX7QzNnPDLAWnLzM-/exec",
   sekolah: "SMP Adiwiyata, Kota Mojokerto",
-
-  // Dashboard mengambil data terbaru setiap 10 detik
   autoRefreshDetik: 10
 };
